dbaa102bedf614aebd17efd2470073ca7abd7fb6
Favour Olukayode <nerosilva522@gmail.com>
2023-02-09 16:01:53 +0100

    chore:..

--- tools.go
+++ tools.go
@@ -2,5 +2,5 @@ package main
 
 import (
 	_ "github.com/golang-migrate/migrate/v4"
-	_ "github.com/golang-migrate/migrate/v4/cmd/migrate"
+	// _ "github.com/golang-migrate/migrate/v4/cmd/migrate"
 )
