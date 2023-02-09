08508c62630f373fb6e962df324b44dbae9e5597
Favour Olukayode <nerosilva522@gmail.com>
2023-02-09 16:06:36 +0100

    fix: fixed up parsing redis address

--- database/cache/redis.go
+++ database/cache/redis.go
@@ -1,13 +1,21 @@
 package cache
 
 import (
+	"log"
+
 	"github.com/redis/go-redis/v9"
 	"github.com/the-Jinxist/busha-assessment/util"
 )
 
 func NewRedis(config util.Config) *redis.Client {
+
+	url, err := redis.ParseURL(config.RedisAddress)
+	if err != nil {
+		log.Fatalf("error occurred while creating redis instance: %s", err)
+	}
+
 	rdb := redis.NewClient(&redis.Options{
-		Addr:     config.RedisAddress,
+		Addr:     url.Addr,
 		Password: "", // no password set
 		DB:       0,  // use default DB
 	})
